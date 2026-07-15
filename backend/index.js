01cfe460f0547f1e485ebbd088c7bccf02565f95
Praniket <praniketkolte03@gmail.com>
2026-07-15 17:36:40 +0530

    Connect MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
-const PORT = 3002;
-
+const PORT = process.env.PORT || 3002;
 
 
 
@@ -17,5 +19,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port ${3002}`);
 });
