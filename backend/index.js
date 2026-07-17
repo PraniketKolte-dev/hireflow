a0e6f7ab50e1aa5643b985ef7b2d72f1ea5d2754
Praniket <praniketkolte03@gmail.com>
2026-07-17 23:18:05 +0530

    Implement user authentication module

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+
 dotenv.config({});
+
 const app = express();
-const PORT = process.env.PORT || 3002;
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +16,12 @@ const corsOptions = {
   origin: "http//localhost:5173",
   credentials: true,
 };
-app.use(cors(corsOptions));
+
+app.use(cors(corsOptions));                             
+
+const PORT = process.env.PORT || 3002;
+
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
