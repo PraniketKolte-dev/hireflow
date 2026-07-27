31ef31d16609097e637f90c6c7e08c8e5e5e1c86
Praniket <praniketkolte03@gmail.com>
2026-07-28 03:26:58 +0530

    Implement company and job management features

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -26,7 +26,7 @@ export const postJob = async (req, res) => {
       !position ||
       !companyId
     ) {
-      return res.status(400)({
+      return res.status(400).json({
         message: "Something is missing.",
         success: false,
       });
@@ -64,9 +64,11 @@ export const getAllJobs = async (req, res) => {
       ],
     };
 
-    const jobs = await Job.find(query).populate({
-        path:"company"
-    }).sort({createdAt:-1});
+    const jobs = await Job.find(query)
+      .populate({
+        path: "company",
+      })
+      .sort({ createdAt: -1 });
     if (!jobs) {
       return res.status(404).json({
         message: "Jobs not found.",
@@ -86,7 +88,9 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
   try {
     const jobId = req.params.id;
-    const job = await Job.findById(jobId);
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+    });
 
     if (!job) {
       return res.status(404).json({
@@ -103,7 +107,10 @@ export const getJobById = async (req, res) => {
 export const getAdminJobs = async (req, res) => {
   try {
     const adminId = req.id;
-    const jobs = await Job.find({ created_by: adminId });
+    const jobs = await Job.find({ created_by: adminId }).populate({
+      path: "company",
+      createdAt: -1,
+    });
     if (!jobs) {
       return res.status(404).json({
         message: "Jobs not found",
@@ -118,6 +125,3 @@ export const getAdminJobs = async (req, res) => {
     console.log(error);
   }
 };
-
-
-
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Jobs from "./components/Jobs";
 import Browse from "./components/Browse";
 import Profile from "./components/Profile";
 import JobDescription from "./components/JobDescription";
+import Companies from "./components/admin/Companies";
+import CompanyCreate from "./components/admin/CompanyCreate";
+import CompanySetup from "./components/admin/CompanySetup";
+import AdminJobs from "./components/admin/AdminJobs"
+import PostJob from "./components/admin/PostJob";
 
 const appRouter = createBrowserRouter([
   {
@@ -26,15 +31,37 @@ const appRouter = createBrowserRouter([
   },
   {
     path: "/description/:id",
-    element: <JobDescription/>
+    element: <JobDescription />,
   },
-   {
+  {
     path: "/browse",
     element: <Browse />,
   },
   {
     path: "/profile",
     element: <Profile />,
+  },
+
+  {
+    path: "/admin/companies",
+    element: <Companies />,
+  },
+
+  {
+    path: "/admin/companies/create",
+    element: <CompanyCreate />,
+  },
+  {
+    path: "/admin/companies/:id",
+    element: <CompanySetup />,
+  },
+   {
+    path: "/admin/jobs",
+    element: <AdminJobs />,
+  },
+  {
+    path: "/admin/jobs/create",
+    element: <PostJob />,
   },
 ]);
 
