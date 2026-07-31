63bdc298af580bb95508dde5571dd29d2a6af718
Praniket <praniketkolte03@gmail.com>
2026-08-01 03:39:05 +0530

    Update CORS origin for production frontend

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://hireflow-vdod.onrender.com",
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3002;
